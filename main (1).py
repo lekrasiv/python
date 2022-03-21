a02d7cd96b9c7f3db844b42790e9cb227f33d638
lekrasiv <101781463+lekrasiv@users.noreply.github.com>
2022-03-21 23:59:13 +0600

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,11 @@
+name = input("Ваше, имя? ")
+
+age = input("Ваша дата рождения? ")
+
+brain = input("Где вы учитесь? ")
+
+print("Ваше имя:",name)
+
+print('Ваш возвраст: ' + age)
+
+print('Вы живете в:',brain)
